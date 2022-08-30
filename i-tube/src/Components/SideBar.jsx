df0a1b297eece1060c8daebae55353124481ef67
Ashish Sahu <ashishsahu10428@gmail.com>
2022-08-30 20:06:05 +0530

    ✨ side bar working implemented

--- i-tube/src/Components/SideBar.jsx
+++ i-tube/src/Components/SideBar.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { categories } from '../Utils/Constant'
 import { Stack } from '@mui/material'
 
-const selectedCategory = 'New';
 
-function SideBar() {
+function SideBar({ selectedCategory, setSelectedCategory }) {
   return (
     <Stack direction="row" sx={{overflowY:"auto", height:{sx:'auto',md:'95%'},flexDirection:{md:'column'},}}>
         {categories.map((category) => (
       <button
         className="category-btn"
-        // onClick={() => setSelectedCategory(category.name)}
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
